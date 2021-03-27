4e27487c82fe8dfad3eb66f04ae1d64c6041e701
Kswooksw0 <kimsangwoothomas@gmail.com>
2021-03-27 21:18:57 +0800

    Bookings and Reviews Paths added

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import Homepage from './components/Homepage.vue';
 import Loginpage from './components/Loginpage.vue';
 import Listings from './components/Listings.vue';
 import IndividualListings from './components/IndividualListings.vue';
+import Bookings from './components/Bookings.vue';
+import Reviews from './components/Reviews.vue';
 
 export default[
     {path: '/', component: Homepage},
     {path: '/listings', component: Listings},
     {path: '/login', component: Loginpage},
-    {path: '/indiv', component: IndividualListings, props: true}
-    
+    {path: '/indiv', component: IndividualListings, props: true},
+    {path: '/bookings', component: Bookings},
+    {path: '/reviews', component: Reviews}
 ]
