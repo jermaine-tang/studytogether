8540bf1d6c7514333184d6cb4fde9501ba4a1122
Kswooksw0 <kimsangwoothomas@gmail.com>
2021-03-27 21:18:32 +0800

    Bookings tab added
    Bookings tab added for navigation but will be removed once we have 'Bookings' under 'My Profile' tab in the nav bar.

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -6,6 +6,7 @@
                 <ul>
                     <li><router-link to="/">Home</router-link></li>
                     <li><a href="/listings">Listings</a></li>
+                    <li><router-link to="/bookings">Bookings</router-link></li>
                     <li><router-link to="/login">Login</router-link></li>
                 </ul>
             </nav>
