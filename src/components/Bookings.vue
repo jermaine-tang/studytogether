3c9a86afefcfebdf832acde37765750cd663cbee
Kswooksw0 <kimsangwoothomas@gmail.com>
2021-04-01 16:23:18 +0800

    Update on Bookings.vue (2)
    Edited so that it now includes pax for each booking and so that the bookings are ordered by date showing the most immediate booking at the top under 'Upcoming' , likewise for those under 'Previous'.
    
    Also edited so that all the bookings are specific to the user in the interface.

--- src/components/Bookings.vue
+++ src/components/Bookings.vue
@@ -2,35 +2,57 @@
 <div id="bookings list">
     <app-header></app-header>
     <h1>Bookings</h1>
-    <ul class="bookings-list">
-        <li v-for="booking in bookings" :key="booking.index">
-            <img :src="booking[4]" alt="picture" id="main-pic">
-            <div id="name"><b>{{booking[5]}}</b></div>
-            <button class="button" v-if="new Date() < new Date(booking[2])">Cancel Booking</button>
-            <button class="button" v-if="new Date() >= new Date(booking[2])" v-bind:id ="booking[1]" v-on:click="route($event)">Leave Review</button>
+    <ul class="upcoming-bookings-list" v-if="newBookings.length > 0">
+        <h3><b>Upcoming</b></h3>
+        <li v-for="booking in newBookings" :key="booking.index">
+            <img :src="booking[5]" alt="picture" id="main-pic">
+            <div id="name"><b>{{booking[6]}}</b></div>
+            <button class="button" v-on:click="cancel(booking.index)">Cancel Booking</button>
+            <div id="pax"><b>Total Coming: {{booking[1]}}</b></div>
             <div id="clockIcon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="12" cy="12" r="9" />
                     <polyline points="12 7 12 12 15 15" />
                 </svg>
-                <span id="time"><b>{{booking[2]}} / {{booking[3]}}</b></span>
+                <span id="time"><b>{{booking[3]}} / {{booking[4]}}</b></span>
             </div>
-            
         </li>
     </ul>
+
+    <ul class="past-booking-list">
+        <h3><b>Previous</b></h3>
+        <li v-for="booking in pastBookings" :key="booking.index">
+            <img :src="booking[5]" alt="picture" id="main-pic">
+            <div id="name"><b>{{booking[6]}}</b></div>
+            <button class="button" v-bind:id ="booking[2]" v-on:click="route($event)">Leave Review</button>
+            <div id="pax"><b>Total Coming: {{booking[1]}}</b></div>
+            <div id="clockIcon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="12" cy="12" r="9" />
+                    <polyline points="12 7 12 12 15 15" />
+                </svg>
+                <span id="time"><b>{{booking[3]}} / {{booking[4]}}</b></span>
+            </div>
+        </li>
+    </ul>
+
 </div>
 </template>
 
 <script>
 import Header from './UI/Header.vue';
 import database from '../firebase.js';
+import firebase from 'firebase';
 
 export default {
     data() {
         return {
-            bookings: [],
-            docArr: [],
+            pastBookings: [],
+            newBookings: [],
+            newBookingsNoDup: [],
+            docArr: []
         }
     },
     methods: {
@@ -43,6 +65,10 @@ export default {
             this.$router.push({path: `reviews/${doc_id}`, query: {id: doc_id}})
         },
 
+        cancel: function(idx) {
+            this.newBookings.splice(idx,1)
+        },
+        
         getDoc: async function() {
             var arr = []
             await database.collection('bookings').get().then(querySnapShot => {
@@ -55,16 +81,28 @@ export default {
 
         fetchItems: async function() {
             this.docArr = await this.getDoc()
-            let myBookings = []
+            var currentUser = firebase.auth().currentUser.uid 
+            let upcoming = []
+            let past = []
+
+            function sortByDate(a, b) {
+                if (new Date(a[3]) == new Date(b[3])) {
+                    return 0;
+                } else {
+                    return (new Date(a[3]) < new Date(b[3])) ? -1 : 1;
+                }
+            }
+
             this.docArr.forEach(async function(doc) {
                 
                 let userid = doc['userid']
                 let location = doc['location']
+                let pax = doc['pax']
                 async function retrieve(locationId) {     
                     var locationData = []
                         await database.collection('listings').doc(locationId).get().then(doc => {
                             let listingData = doc.data()
-                            locationData.push(listingData['photoURL2'])
+                            locationData.push(listingData['photoURL1'])
                             locationData.push(listingData['name'])
                         })
                     return locationData
@@ -76,13 +114,20 @@ export default {
                 let start = time[0].slice(0,4)
                 let end = time.pop().slice(-4)
                 let duration = start + ' - ' + end
-                let booking = [userid, location, dateOfVisit, duration]
+                let booking = [userid, pax, location, dateOfVisit, duration]
                 let combined = [...booking, ...locationAndName]
-                myBookings.push(combined)
+    
+                if(new Date() < new Date(combined[3]) && combined[0] == currentUser) {
+                    upcoming.push(combined)
+                    upcoming.sort(sortByDate)
+                } else if(combined[0] == currentUser) {
+                    past.push(combined)
+                    upcoming.sort(sortByDate)
+                }
             })
-            this.bookings = myBookings
-            console.log(this.bookings)
-        }
+            this.newBookings = upcoming
+            this.pastBookings = past
+        },
     },
 
     components: {
@@ -138,15 +183,22 @@ li {
     font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 
+#pax {
+    position: relative;
+    left: 235px;
+    bottom: 70px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 15px;
+}
+
 #clockIcon {
     position: absolute;
-    top: 100px;
+    top: 120px;
     left: 235px;    
 }
 
 #main-pic {
   position: relative;
-  /* left: 1px; */
   border-radius: 15px;
   width: 200px;
   height: 150px;
