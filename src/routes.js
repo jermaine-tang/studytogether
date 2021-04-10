1c43ae3b3c37b5aea0849cb393b1d4e7e2df6b42
KoKoLeeee <jaredlee33@hotmail.com>
2021-04-10 16:25:12 +0800

    navigation guard

--- src/routes.js
+++ src/routes.js
@@ -9,22 +9,79 @@ import ForgotPassword from './components/ForgotPassword.vue';
 import Favourites from './components/Favourites.vue';
 import Reservation from './components/Reservation.vue';
 import MyBusiness from './components/Business/MyBusiness.vue';
-import BusinessLogin from './components/Business/BusinessLogin.vue';
 import BusinessSignup from './components/Business/BusinessSignup.vue';
 
-export default[
-    {path: '/', component: Homepage},
-    {path: '/listings', component: Listings},
-    {path: '/listings/:id', name: 'indiv', component: IndividualListings, props:true},
-    {path: '/login', component: Loginpage},
-    {path: '/signup', component: SignupPage},
+// import firebase from 'firebase'
+
+export default [
+    {
+        path: '/',
+        component: Homepage,
+    },
+    {
+        path: '/listings',
+        component: Listings
+    },
+    {
+        path: '/listings/:id',
+        name: 'indiv',
+        component: IndividualListings,
+        props: true
+    },
+    {
+        path: '/login',
+        component: Loginpage
+    },
+    {
+        path: '/signup',
+        component: SignupPage
+    },
     // {path: '/indiv', component: IndividualListings, props: true},
-    {path: '/bookings', component: Bookings},
-    {path: '/forgotpassword', component: ForgotPassword},
-    {path: '/favourites', name: "favourites", component: Favourites},
-    {path: '/reviews/:id', component: Reviews, props: true},
-    {path: '/listings/:id/reserve', name: 'reservation', component: Reservation, props:true},
-    {path: '/mybiz', name: 'business', component: MyBusiness},
-    {path: '/bizlogin', component: BusinessLogin},
-    {path: '/bizsignup', component: BusinessSignup}
+    {
+        path: '/bookings',
+        component: Bookings
+    },
+    {
+        path: '/forgotpassword',
+        component: ForgotPassword,
+        meta: { auth: 'customer' }
+    },
+    {
+        path: '/favourites',
+        name: "favourites",
+        component: Favourites,
+        meta: { auth: 'customer' }
+    },
+    {
+        path: '/reviews/:id',
+        component: Reviews,
+        props: true,
+        meta: { auth: 'customer' },
+    },
+    {
+        path: '/listings/:id/reserve',
+        name: 'reservation',
+        component: Reservation,
+        props: true,
+        meta: { auth: 'customer' },
+        // beforeEnter: (to, from, next) => {
+        //     firebase.auth().onAuthStateChanged(user => {
+        //         if (user) {
+        //             next();
+        //         } else {
+        //             next('/login');
+        //         }
+        //     })
+        // }
+    },
+    {
+        path: '/mybiz',
+        name: 'business',
+        component: MyBusiness,
+        meta: { auth: 'owner' }
+    },
+    {
+        path: '/bizsignup',
+        component: BusinessSignup
+    }
 ]
