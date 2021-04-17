de20d25aa359134f26ef036be54f4ede86d9ac7d
Kswooksw0 <kimsangwoothomas@gmail.com>
2021-04-18 06:40:21 +0800

    Added function to segment timings into intervals

--- src/components/Bookings.vue
+++ src/components/Bookings.vue
@@ -5,13 +5,13 @@
     <div class="table" v-if="newBookings.length > 0">
         <ul>
             <h3><b>Upcoming</b></h3>
-            <li v-for="booking in newBookings" :key="booking.index">
+            <li v-for="(booking,key) in newBookings" :key="booking.index">
                 <div id="name"><b>{{booking[6]}}</b></div>
                 <br>
                 <!-- picture -->
                 <img :src="booking[5]" alt="picture" id="main-pic">
                 <!-- button -->
-                <button href="#" class="btn" v-bind:id="booking[7]" v-on:click="cancel(booking.index); del($event)">
+                <button href="#" class="btn" v-bind:val="booking[7]" v-on:click="cancel(key); del($event)">
                     Cancel Booking
                 </button>
                 <!-- pax -->
@@ -23,7 +23,12 @@
                         <circle cx="12" cy="12" r="9" />
                         <polyline points="12 7 12 12 15 15" />
                     </svg>
-                    <span id="time"><b>{{booking[3]}} / {{booking[4]}}</b></span>
+                    <span id="date"><b>{{booking[3]}}</b></span>
+                    <ul id="time">
+                         <li id="timings" v-for="timing in booking[4]" :key="timing.index">
+                             {{timing}}
+                        </li>
+                    </ul>                   
                 </div>
             </li>
         </ul>
@@ -51,7 +56,12 @@
                         <circle cx="12" cy="12" r="9" />
                         <polyline points="12 7 12 12 15 15" />
                     </svg>
-                    <span id="time"><b>{{booking[3]}} / {{booking[4]}}</b></span>
+                    <span id="date"><b>{{booking[3]}}</b></span>
+                    <ul id="time">
+                         <li id="timings" v-for="timing in booking[4]" :key="timing.index">
+                             {{timing}}
+                        </li>
+                    </ul> 
                 </div>
             </li>
         </ul>
@@ -80,16 +90,80 @@ export default {
         },
 
         route: function(event) {
-            console.log(event)
             let doc_id = event.target.getAttribute("id");
-
-            console.log(doc_id)
-            console.log(this.pastBookings)
             this.$router.push({path: `reviews/${doc_id}`, query: {id: doc_id}})
         },
 
-        del: function(event) {
-            let id = event.target.getAttribute("id")
+        visitDateToString: function(dateOfVisit) {
+            // dateOfVisit == Apr 17 2021
+            // month == Apr
+            let month = dateOfVisit.slice(0,3)
+            // day == 17
+            let day = dateOfVisit.slice(4,6)
+            // year == 21
+            let year = dateOfVisit.slice(-2)
+
+            let monthString = ''
+            let dayString = day
+            let yearString = year
+
+            if (month == 'Jan') {
+                monthString = '01'
+            } else if (month == 'Feb') {
+                monthString = '02'
+            } else if (month == 'Mar') {
+                monthString = '03'
+            } else if (month == 'Apr') {
+                monthString = '04'
+            } else if (month == 'May') {
+                monthString = '05'
+            } else if  (month == 'Jun') {
+                monthString = '06'
+            } else if (month == 'Jul') {
+                monthString = '07'
+            } else if (month == 'Aug') {
+                monthString = '08'
+            } else if (month == 'Sep') {
+                monthString = '09'
+            } else if (month == 'Oct') {
+                monthString = '10'
+            } else if (month == 'Nov') {
+                monthString = '11'
+            } else if (month == 'Dec') {
+                monthString = '12'
+            }
+            return dayString + monthString + yearString
+        },
+
+        del: async function(event) {
+            let values = event.target.getAttribute("val")
+            let id = values.split(',')[0]
+            let loc = values.split(',')[1]
+            let pax = values.split(',')[2]
+            let dateOfVisit = values.split(',')[3]
+            let stringOfVisitDate = this.visitDateToString(dateOfVisit)
+
+            async function getTime() {                
+                let time = []
+                await database.collection('bookings').doc(id).get().then(doc => {
+                    let data = doc.data()
+                    let timeArr = data['time']
+                    time = timeArr
+                })
+                return time
+            }
+
+            let arrOfBookings = await getTime()
+
+            const increasePax = firebase.firestore.FieldValue.increment(pax)
+            const timeslots = database.collection('listings').doc(loc).collection('timeslots').doc(stringOfVisitDate)
+        
+            for(var booking of arrOfBookings) {
+                console.log(booking)
+                timeslots.update({
+                    [booking] :increasePax
+                })
+            }
             database.collection('bookings').doc(id).delete().then(() => console.log('delete success'))
         },
 
@@ -131,7 +205,38 @@ export default {
                     return (new Date(a[3]) > new Date(b[3])) ? -1 : 1;
                 }
             }
+            // sorts the timings in the 'time' array in firestore. 
+            // if the array is ['0900 - 1000', '0800 - 0900', '1400 - 1500','1300 - 1400'],
+            // it returns ['0800 - 0900', '0900 - 1000', '1300 - 1400','1400 - 1500']
+            function sortByTiming(a, b) {
+                if (Number(a.slice(0,2)) == Number(b.slice(0,2))) {
+                    return 0;
+                } else {
+                    return (Number(a.slice(0,2)) < Number(b.slice(0,2))) ? -1 : 1;
+                }
+            }
+            // segements the timings into intervals. 
+            // E.g if the sorted array is ['0800 - 0900', '0900 - 1000', '1000 - 1100', '1300 - 1400','1400 -1500']
+            // it returns ['0800 - 1100', '1300 - 1500']
+            function segmentToIntervals(arr) {
+                var intervals = []
+                var currTime = arr[0].slice(0,4)
 
+                for (var i = 0; i < arr.length; i++) {
+                    if (i == arr.length - 1) {
+                        currTime = currTime + ' - ' + arr[i].slice(-4)
+                        intervals.push(currTime)
+                    } else if (arr[i].slice(-4) != arr[i + 1].slice(0,4)) {
+                        currTime = currTime + ' - ' + arr[i].slice(-4)
+                        intervals.push(currTime)
+                        currTime = arr[i + 1].slice(0,4)
+                    } else {
+                        continue
+                    }
+                }
+                return intervals
+            }
+            
             this.docArr.forEach(async function(doc) {
                 let id = [doc['id']]
                 let userid = doc['userid']
@@ -149,12 +254,17 @@ export default {
                 let locationAndName = await retrieve(location)                
                 let date = doc['date'].toDate().toString()
                 let dateOfVisit = date.slice(4,15)
+                // get the array of timings booked
                 let time = doc['time']
-                let start = time[0].slice(0,4)
-                let end = time.pop().slice(-4)
-                let duration = start + ' - ' + end
+                // sort the array
+                let sortedTimings = time.sort(sortByTiming)
+                // get all the intervals booked by the user and store into an array called 'duration'
+                let duration = segmentToIntervals(sortedTimings)
+                // v-bind values
+                let vbindValues = [...id, location, pax, dateOfVisit]
+
                 let booking = [userid, pax, location, dateOfVisit, duration]
-                let combined = [...booking, ...locationAndName,...id]
+                let combined = [...booking, ...locationAndName, vbindValues]
     
                 if(new Date() < new Date(combined[3]) && combined[0] == currentUser) {
                     upcoming.push(combined)
@@ -180,8 +290,6 @@ export default {
 </script>
 
 <style scoped>
-
-
 
 ul {
   /* display: flex; 
@@ -268,19 +376,30 @@ h3 {
     transition: 0.2s;
 }
 
+#time {
+    display: inline-block;
+    list-style-type: none;
+    padding-left: 0px;
+}
+
+#timings {
+    display: inline-block;
+    border: none;
+}
+
 
 #pax {
     position: relative;
     left: 10%;
-    bottom: 140px;
+    bottom: 220px;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: 15px;
 }
 
 #clockIcon {
     position: relative;
-    left: 20%;
-    bottom: 100px;
+    left: 10%;
+    bottom: 200px;
 }
 
 #main-pic {
@@ -301,7 +420,7 @@ h3 {
     font-weight: 1000;
 }
 
-#time {
+#date {
     position: relative;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: 16px;
