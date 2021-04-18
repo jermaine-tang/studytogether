bfb31215e63fd30d93f46979a064437eb6b16a0c
Kswooksw0 <kimsangwoothomas@gmail.com>
2021-04-18 12:49:09 +0800

    Bug Fix
    It now only loads the bookings that are either for today or tomorrow onwards instead of all bookings.

--- src/components/Business/Biz Bookings.vue
+++ src/components/Business/Biz Bookings.vue
@@ -5,8 +5,10 @@
     <br><br>
     <div id="mybizbookings">
         <ul>
+            <!-- E.g bookings = ['dateObject', '18 Apr 2021', [ ['Jermaine',3,['0700 - 0800', '1300 - 1500']], ['Jared',5, ['1100 - 1200']] ] ] -->
             <li v-for="booking in bookings" :key="booking.index">
                 <!-- date of coming -->
+                <!-- E.g 18 Apr 2021  -->
                 <div id="dateComing">
                     {{booking[1]}}
                 </div>
@@ -14,22 +16,22 @@
                 <!-- iterations of all customers coming on that date -->
                 <div>
                     <ul class="customers">
+                        <!-- booking[2] == [ ['Jermaine',3,['0700 - 0800', '1300 - 1500']], ['Jared',5, ['1100 - 1200']] ] -->
                         <li id="customer" v-for="customer in booking[2]" :key="customer.index">
                             <!-- customer name and pax -->
+                            <!-- 'Jermaine' (pax: 3) -->
                             <div id="customerName">{{customer[0]}}  (pax: {{customer[1]}})  </div>
-                            <!-- customer[2] refers to duration which is an array -->
-                            <!-- E.g ['0700 - 1000', '1100 - 1200', '1300 - 1500'] -->
                             <br>
                             <ul>
+                                <!-- customer[2] == ['0700 - 0800', '1300 - 1500'] -->
                                 <li v-for="interval in customer[2]" :key="interval.index">
-                                    <!-- E.g '0700 - 1000' -->
+                                    <!-- '0700 - 0800' -->
                                     {{interval}}
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
-
             </li>
         </ul>
     </div>
@@ -54,10 +56,10 @@ export default {
 
         getDoc: async function() {
             // hardcode
-            var userId = '5iBl58sV6uv7riUzCQzn'
-
+            // var userId = "fo2v2d2wA8Q2k3tXlt8hTxiRUcs2"
+            
             // CurrentUSer.uid == listing doc ID
-            // var userId = firebase.auth().currentUser.uid
+            var userId = firebase.auth().currentUser.uid
             var arr = []
             await database.collection('listings').doc(userId).collection('timeslots').get().then(querySnapShot => {
                 let slot = {}
@@ -72,7 +74,7 @@ export default {
         fetchItems: async function() {
             this.timeslots = await this.getDoc()
             var listOfBookings = []
-           
+           // function to sort the dates in order, most immedate date coming first
             function sortByDate(a, b) {
                 if (a[0] == b[0]) {
                     return 0;
@@ -80,7 +82,8 @@ export default {
                     return (a[0] < b[0]) ? -1 : 1;
                 }
             }
-
+            // sorts the timings, with ealiest timing coming first. 
+            // E.g [1100 - 1200, 0800 - 0900] into [0800 - 0900, 1100 - 1200]
             function sortByTiming(a, b) {
                 if (Number(a.slice(0,2)) == Number(b.slice(0,2))) {
                     return 0;
@@ -88,7 +91,8 @@ export default {
                     return (Number(a.slice(0,2)) < Number(b.slice(0,2))) ? -1 : 1;
                 }
             }
-
+            // segments the timings into intervals. 
+            // E.g [1100 - 1200, 1200 - 1300, 1400 - 1500] into [1100 - 1300, 1400 - 1500]
             function segmentToIntervals(arr) {
                 var intervals = []
                 var currTime = arr[0].slice(0,4)
@@ -109,12 +113,17 @@ export default {
             }
 
             this.timeslots.forEach(async function(slot) {
+                // E.g '170421'
                 let timeslot = slot['id']
-                let day = timeslot.slice(0,2) // 17
-                let month = timeslot.slice(2,4) // 04
-                let year = timeslot.slice(4,6) // 21
+                // 17
+                let day = timeslot.slice(0,2) 
+                // 04
+                let month = timeslot.slice(2,4) 
+                // 21
+                let year = timeslot.slice(4,6) 
+                //  '2021-04-17'
                 let dateInput = '20' + year + '-' + month + '-' + day
-                
+                // logic to achieve date domain
                 let dateObj = new Date(dateInput)
                 let dateObj_copy = new Date(dateInput)
                 dateObj_copy.setHours(dateObj_copy.getHours() - 8);
@@ -122,28 +131,37 @@ export default {
                 let dateObjdayForward = new Date(dateObj.getTime())
                 dateObjdayForward.setDate(dateObjdayForward.getDate() + 1)
                 dateObjdayForward.setHours(dateObjdayForward.getHours() - 8)
-                
+                // lower limit of the date domain 
+                // E.g 18 Apr 2021 00:00:00
                 let timeObj = firebase.firestore.Timestamp.fromDate(dateObj_copy)
+                // upper limit of the date domain
+                // E.g 19 Apr 2021 00:00:00
                 let timeObjLimit = firebase.firestore.Timestamp.fromDate(dateObjdayForward)
+
                 let outputDate = dateObj.toDateString()
                 let outputDay = outputDate.slice(-7,-5)
                 let outputYear = outputDate.slice(-4)
                 let outputMonth = outputDate.slice(-11,-8)
+                // E.g '18 Apr 2021'
                 let date = outputDay + ' ' + outputMonth + ' ' + outputYear
-
+                // E.g 'Apr 18 2021'
+                let dateToCompare = outputMonth + ' ' + outputDay + ' ' + outputYear
+                // get current date to string. E.g Sun Apr 18 2021
+                let currentDate = new Date().toDateString()
+                // E.g Apr 18 2021
+                let currentDateWithoutDayOfWeek = currentDate.substr(currentDate.indexOf(' ') + 1)
+                // collects details of all customers coming on the date which falls between startDate and endDate (the date domain)
                 async function getNames(startDate, endDate) {
-
                     var customerinfos = []
                         await database.collection('bookings').where("date", ">=", startDate).where("date", "<", endDate).get().then((querySnapShot) => {
                             console.log(startDate.toDate().toDateString())
                             querySnapShot.docs.forEach(async function(doc) {
-
                                 let data = doc.data()
                                 let customerName = data['name']
                                 let time = data['time']
-                                // let start = time[0].slice(0,4)
-                                // let end = time.pop().slice(-4)
+                                // sort the timings in order, starting from the earliest timing
                                 time = time.sort(sortByTiming)
+                                // then segment them into intervals
                                 let duration = segmentToIntervals(time)
                                 let pax = data['pax']
                                 let customer = [customerName, pax, duration]
@@ -153,8 +171,9 @@ export default {
                     return customerinfos
                 }
                 let customersDetails = await getNames(timeObj, timeObjLimit)
-               
-                if(customersDetails.length != 0) {
+                // only add each booking to the array if there are customer details and the date of coming 
+                // is either today or tomorrow onwards
+                if(customersDetails.length != 0 && (dateToCompare == currentDateWithoutDayOfWeek || new Date() < new Date(dateInput))) {
                     let booking = [dateObj, date, customersDetails]
                     listOfBookings.push(booking)
                     listOfBookings.sort(sortByDate)
